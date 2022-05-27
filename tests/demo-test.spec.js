72dd5383057083ee5a5c4a93a9c1554783102fdd
Euan Black <euanb@forrit.com>
2022-05-27 11:48:17 +0100

    Initial project setup - initialise playwright, create an example test and installation of various packages

---
+++ tests/demo-test.spec.js
@@ -0,0 +1,11 @@
+// Test specification for testing https://www.demoblaze.com/index.html
+
+const { test, expect } = require ('@playwright/test');
+
+test.beforeEach(async ({ page }) =>{
+    await page.goto('https://www.demoblaze.com/index.html');
+});
+
+test('verify the logo is present', async ({ page }) => {
+await expect(page.locator('#navag')).toBeVisible();
+});
